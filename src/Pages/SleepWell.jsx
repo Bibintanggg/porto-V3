2223a9760888f0e9e23d3b09ab4b75779eb4a9f7
Bibintanggg <bintangyudhaa8@gmail.com>
2025-01-18 23:58:02 +0700

    Modify DarkMode in Project Sleepwell

--- src/Pages/SleepWell.jsx
+++ src/Pages/SleepWell.jsx
@@ -1,6 +1,7 @@
 import { data, useNavigate } from "react-router-dom";
 import StarIcon from "../assets/Icon/star.svg"
 import Sidebar from "../Component/Sidebar";
+import { useDarkMode } from "../Component/DarkMode";
 
 const valueProject = [
     {
@@ -22,12 +23,13 @@ const valueProject = [
 ]
 
 function SleepWell({ sleepWellProject }) {
+    const {darkMode, toggleDarkMode} = useDarkMode()
     const navigate = useNavigate();
     const handleNavigate = (path) => {
         navigate(path);
     }
     return (
-        <div className="relative min-h-screen h-screen flex">
+        <div className={`relative min-h-screen h-screen flex ${darkMode ? 'text-white bg-[#131523]' : 'text-black bg-[#f2f2fc]'}`}>
             <div className="w-[250px] h-screen fixed top-0 left-0">
                 <Sidebar />
             </div>
@@ -35,26 +37,30 @@ function SleepWell({ sleepWellProject }) {
             <div className="ml-[250px] w-full overflow-y-auto">
                 <div className="bg-transparent p-8">
                     <div className="flex justify-between text-left">
-                        <h1 className="text-base font-semibold text-gray-800 font-poppins pt-2">
+                        <h1 className={`text-base font-semibold text-gray-800 font-poppins pt-2 
+                            ${darkMode ? 'text-white' : 'text-black'}`}>
                             PROJECT
                         </h1>
                         <div className="text-right">
-                            <h2 className="text-base font-semibold text-gray-800 font-poppins">
+                            <h2 className={`text-base font-semibold text-gray-800 font-poppins 
+                                ${darkMode ? 'text-white' : 'text-black'}`}>
                                 Hello, Bintang!
                             </h2>
                             <p className="-translate-y-2">Role &gt; Student</p>
                         </div>
                     </div>
-                    <div className="w-full h-0.5 bg-black mx-auto opacity-25"></div>
+                    <div className={`w-full h-0.5 bg-black mx-auto opacity-25
+                        ${darkMode ? 'bg-white' : 'bg-black'}`}></div>
 
                     <div className="pt-7 flex gap-2">
-                        <button className="bg-slate-300 w-24 h-10 rounded-lg font-jakarta"
+                        <button className={` w-24 h-10 rounded-lg font-jakarta
+                        ${darkMode ? 'text-white bg-gradient-to-r from-black/30 to-black/30' : 'text-black bg-white'}`}
                         onClick={() => handleNavigate("/project")}>
                             Kembali
                         </button>
 
                         <div className="items-center flex font-jakarta gap-2">
-                            <p className="text-opacity-40 text-black">Project</p>
+                            <p className={`text-opacity-40 text-black ${darkMode ? 'text-white' : 'text-black'}`}>Project</p>
                             <span>&gt; Website Kesehatan Tidur</span>
                         </div>
                     </div>
@@ -65,7 +71,7 @@ function SleepWell({ sleepWellProject }) {
                                 <h1 className="text-4xl font-poppins font-bold block text-balance">
                                     {sleepWellProject.title}
                                 </h1>
-                                <div className="w-36 h-1 rounded-full bg-black mt-2 animate"></div>
+                                <div className={`w-36 h-1 rounded-full mt-2 animate ${darkMode ? 'bg-white' : 'bg-black'}`}></div>
                             </div>
 
                             <div className="pt-10">
@@ -73,14 +79,16 @@ function SleepWell({ sleepWellProject }) {
                                     {sleepWellProject.desc}
                                 </p>
 
-                                <div className="flex gap-5 pt-10 w-[30rem] h-[6rem] justify-center items-center rounded-lg bg-gray-50 mt-10">
-                                    <div className="relative bg-white w-52 h-14 rounded-lg mb-10 overflow-hidden group">
-                                        <div className="absolute top-0 left-0 h-full bg-slate-200 w-0 
-                      transition-all duration-500 group-hover:w-full"></div>
+                                <div className={`flex gap-5 pt-10 w-[30rem] h-[6rem] justify-center items-center rounded-lg mt-10
+                                    ${darkMode ? 'text-white bg-gradient-to-r from-black/30 to-black/10' : 'text-black bg-white'}`}>
+                                    <div className={`relative w-52 h-14 rounded-lg mb-10 overflow-hidden group 
+                                        ${darkMode ? 'bg-gradient-to-r from-black/50 to-black/30' : 'text-black bg-gray-100'}`}>
 
                                         <div className="relative flex items-center gap-3 text-sm ml-4">
-                                            <div className="bg-slate-300 rounded-full w-10 h-10 flex items-center justify-center mt-auto">
-                                                <img src={sleepWellProject.image2_tech} alt="" className="w-7" />
+                                            <div className={`rounded-full w-10 h-10 flex items-center justify-center mt-auto 
+                                                ${darkMode ? 'bg-gradient-to-r from-black/60' : 'text-black bg-gray-100'}`}>
+                                                <img src={sleepWellProject.image2_tech} alt="" className={`w-7 
+                                                    ${darkMode ? 'invert brightness-75' : ''}`} />
                                             </div>
                                             <div className="flex flex-col mt-2">
                                                 <h2 className="relative z-10">{sleepWellProject.total_tech}</h2>
@@ -89,7 +97,8 @@ function SleepWell({ sleepWellProject }) {
                                         </div>
                                     </div>
 
-                                    <div className="relative bg-white w-52 h-14 rounded-lg mb-10 overflow-hidden group">
+                                    <div className={`relative w-52 h-14 rounded-lg mb-10 overflow-hidden group
+                                        ${darkMode ? 'bg-gradient-to-r from-black/50 to-black/30' : 'text-black'}`}>
                                         <div className="absolute top-0 left-0 h-full bg-slate-200 w-0 
                       transition-all duration-500 group-hover:w-full"></div>
 
