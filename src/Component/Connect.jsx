d4b256f56888d91af447a384c1afea57ad9ef4ce
Rayfryu <rayhanalayyubi2021@gmail.com>
2025-01-24 11:31:56 +0700

    Add link to instagram

--- src/Component/Connect.jsx
+++ src/Component/Connect.jsx
@@ -1,47 +1,108 @@
 import React from "react";
 
-const Connect = ({ title, darkMode, platforms }) => {
+const Connect = ({ title, darkMode, platforms, link }) => {
   return (
-    <div className="mt-10 flex justify-center font-poppins">
-      <div className={`w-[35rem] h-[30rem] rounded-lg ${darkMode ? "" : ""}`}>
+    <div className=" flex justify-center font-poppins">
+      <div
+        className={`w-[35rem] min-h-[1rem] rounded-lg ${darkMode ? "" : ""}`}
+      >
         <div className="p-10">
-          <h1 className={`text-xl font-semibold ${darkMode ? "text-white" : "text-black"}`}>
+          <h1
+            className={`text-xl font-semibold ${
+              darkMode ? "text-white" : "text-black"
+            }`}
+          >
             {title}
-          </h1>
-
-          <div className="mt-4">
-                      <div className={`w-full h-24 isolate aspect-video bg-white/20 shadow-lg ${darkMode ? "" : "bg-gray-100"} rounded-lg flex items-center p-4`}>
-              <div className={`w-12 h-12 rounded-lg flex items-center justify-center 
-                isolate aspect-video bg-white/20 shadow-lg ${darkMode ? "bg-gray-600" : "bg-white"}`}>
-                <img src={platforms[0].icon} className="w-6 h-6" alt={platforms[0].title} />
+          </h1>{" "}
+          .
+          <button className="mt-4 flex items-center" onClick={link}>
+            <div
+              className={`w-[25rem] h-24 isolate aspect-video bg-white/20 shadow-lg ${
+                darkMode ? "" : "bg-gray-100"
+              } rounded-lg flex items-center p-4`}
+            >
+              <div
+                className={`w-12 h-12 rounded-lg flex items-center justify-center 
+                isolate aspect-video bg-white/20 shadow-lg ${
+                  darkMode ? "bg-gray-600" : "bg-white"
+                }`}
+              >
+                <img src={platforms[0].icon} className="w-6 h-6" />
               </div>
               <div className="ml-4">
-                <h1 className={`${darkMode ? "text-white" : "text-black"}`}>{platforms[0].title}</h1>
-                <span className={`text-sm ${darkMode ? "text-gray-400" : "text-gray-600"}`}>{platforms[0].description}</span>
+                <h1 className={`${darkMode ? "text-white" : "text-black"}`}>
+                  {platforms[0].title}
+                </h1>
+                <span
+                  className={`text-sm ${
+                    darkMode ? "text-gray-400" : "text-gray-600"
+                  }`}
+                >
+                  {platforms[0].description}
+                </span>
               </div>
             </div>
-          </div>
-
+          </button>
           <div className="mt-6 flex gap-4">
-            <div className={`w-full h-24 isolate aspect-video bg-white/20 shadow-lg   ${darkMode ? "" : "bg-gray-100"} rounded-lg flex items-center p-4`}>
-              <div className={`w-12 h-12 rounded-lg flex items-center justify-center 
-                isolate aspect-video bg-white/20 shadow-lg ${darkMode ? "" : "bg-white"}`}>
-                <img src={platforms[1].icon} className="w-6 h-6" alt={platforms[1].title} />
+            <div
+              className={`w-full h-24 isolate aspect-video bg-white/20 shadow-lg   ${
+                darkMode ? "" : "bg-gray-100"
+              } rounded-lg flex items-center p-4`}
+            >
+              <div
+                className={`w-12 h-12 rounded-lg flex items-center justify-center 
+                isolate aspect-video bg-white/20 shadow-lg ${
+                  darkMode ? "" : "bg-white"
+                }`}
+              >
+                <img
+                  src={platforms[1].icon}
+                  className="w-6 h-6"
+                  alt={platforms[1].title}
+                />
               </div>
               <div className="ml-2">
-                <h1 className={`${darkMode ? "text-white" : "text-black"}`}>{platforms[1].title}</h1>
-                <span className={`text-sm ${darkMode ? "text-gray-400" : "text-gray-600"}`}>{platforms[1].description}</span>
+                <h1 className={`${darkMode ? "text-white" : "text-black"}`}>
+                  {platforms[1].title}
+                </h1>
+                <span
+                  className={`text-sm ${
+                    darkMode ? "text-gray-400" : "text-gray-600"
+                  }`}
+                >
+                  {platforms[1].description}
+                </span>
               </div>
             </div>
 
-            <div className={`w-full h-24 isolate aspect-video bg-white/20 shadow-lg ${darkMode ? "" : "bg-gray-100"} rounded-lg flex items-center p-4`}>
-                <div className={`w-12 h-12 rounded-lg flex items-center justify-center 
-                isolate aspect-video bg-white/20 shadow-lg ${darkMode ? "" : "bg-white"}`}>
-                <img src={platforms[2].icon} className="w-6 h-6" alt={platforms[2].title} />
+            <div
+              className={`w-full h-24 isolate aspect-video bg-white/20 shadow-lg ${
+                darkMode ? "" : "bg-gray-100"
+              } rounded-lg flex items-center p-4`}
+            >
+              <div
+                className={`w-12 h-12 rounded-lg flex items-center justify-center 
+                isolate aspect-video bg-white/20 shadow-lg ${
+                  darkMode ? "" : "bg-white"
+                }`}
+              >
+                <img
+                  src={platforms[2].icon}
+                  className="w-6 h-6"
+                  alt={platforms[2].title}
+                />
               </div>
               <div className="ml-2">
-                <h1 className={`${darkMode ? "text-white" : "text-black"}`}>{platforms[2].title}</h1>
-                <span className={`text-sm ${darkMode ? "text-gray-400" : "text-gray-600"}`}>{platforms[2].description}</span>
+                <h1 className={`${darkMode ? "text-white" : "text-black"}`}>
+                  {platforms[2].title}
+                </h1>
+                <span
+                  className={`text-sm ${
+                    darkMode ? "text-gray-400" : "text-gray-600"
+                  }`}
+                >
+                  {platforms[2].description}
+                </span>
               </div>
             </div>
           </div>
--- src/Pages/Contact.jsx
+++ src/Pages/Contact.jsx
@@ -1,41 +1,43 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 import Sidebar from "../Component/Sidebar";
 import ShareContactIcon from "../assets/Icon/shareContact.svg";
-import LinkedinIcon from "../assets/Icon/linkedin.svg"
-import GithubIcon from "../assets/TechIcon/GitHub.svg"
-import InstagramIcon from "../assets/Icon/instagram.svg"
+import LinkedinIcon from "../assets/Icon/linkedin.svg";
+import GithubIcon from "../assets/TechIcon/GitHub.svg";
+import InstagramIcon from "../assets/Icon/instagram.svg";
 import { useDarkMode } from "../Component/DarkMode";
 import Input from "../Component/Input";
-import Connect from '../Component/Connect';
+import Connect from "../Component/Connect";
+import { useNavigate } from "react-router-dom";
 
 const platforms = [
   {
     icon: LinkedinIcon,
     title: "Linkedin",
-    description: "Lets Networking On Me"
+    description: "Lets Networking On Me",
+    link: "https://www.instagram.com/bintang.ydha_/",
   },
   {
     icon: GithubIcon,
     title: "Github",
-    description: "Check Out My Projects"
+    description: "Check Out My Projects",
   },
   {
     icon: InstagramIcon,
     title: "Instagram",
-    description: "Check Out My Projects"
+    description: "Check Out My Projects",
   },
-]
+];
 function Contact() {
   const { darkMode } = useDarkMode();
   const [formData, setFormData] = useState({
-    username: '',
-    email: '',
-    message: ''
+    username: "",
+    email: "",
+    message: "",
   });
 
   const [errors, setErrors] = useState({
-    username: '',
-    email: '',
+    username: "",
+    email: "",
   });
 
   const handleChange = (e) => {
@@ -47,74 +49,125 @@ function Contact() {
 
     setErrors({
       ...errors,
-      [name]: '',
+      [name]: "",
     });
   };
 
   const handleSubmit = (e) => {
     e.preventDefault();
     const newErrors = {};
-    if (!formData.username) newErrors.username = 'Username is required';
-    if (!formData.email) newErrors.email = 'Email is required';
+    if (!formData.username) newErrors.username = "Username is required";
+    if (!formData.email) newErrors.email = "Email is required";
 
     if (Object.keys(newErrors).length > 0) {
       setErrors(newErrors);
     } else {
-      console.log('Form Data:', formData);
+      console.log("Form Data:", formData);
     }
   };
 
+  const navigate = useNavigate(link);
+  const handleNavigate = () => {
+    navigate(link);
+  };
   return (
-    <div className={`relative min-h-screen ${darkMode ? 'bg-[#131523]' : 'bg-[#f2f2fc]'}`}>
+    <div
+      className={`relative min-h-screen ${
+        darkMode ? "bg-[#131523]" : "bg-[#f2f2fc]"
+      }`}
+    >
       <div className="flex relative z-10">
         <div
-          className={`w-64 h-screen fixed top-0 left-0 ${darkMode ? 'bg-[#131523]' : 'bg-[#f2f2fc]'}`}
+          className={`w-64 h-screen fixed top-0 left-0 ${
+            darkMode ? "bg-[#131523]" : "bg-[#f2f2fc]"
+          }`}
         >
           <Sidebar />
         </div>
 
         <div className="flex-1 bg-transparent p-8 ml-64">
           <div className="flex justify-between text-left">
-            <h1 className={`text-base font-semibold ${darkMode ? 'text-white' : 'text-gray-800'} font-poppins pt-2`}>
+            <h1
+              className={`text-base font-semibold ${
+                darkMode ? "text-white" : "text-gray-800"
+              } font-poppins pt-2`}
+            >
               Contact
             </h1>
             <div className="text-right">
-              <h2 className={`text-base font-semibold ${darkMode ? 'text-white' : 'text-gray-800'} font-poppins`}>
+              <h2
+                className={`text-base font-semibold ${
+                  darkMode ? "text-white" : "text-gray-800"
+                } font-poppins`}
+              >
                 Hello, Bintang !
               </h2>
-              <p className={`-translate-y-2 ${darkMode ? 'text-gray-300' : 'text-gray-600'}`}>
+              <p
+                className={`-translate-y-2 ${
+                  darkMode ? "text-gray-300" : "text-gray-600"
+                }`}
+              >
                 Role &gt; Student
               </p>
             </div>
           </div>
 
-          <div className={`w-full h-0.5 ${darkMode ? 'bg-white' : 'bg-black'} mx-auto opacity-25`}></div>
+          <div
+            className={`w-full h-0.5 ${
+              darkMode ? "bg-white" : "bg-black"
+            } mx-auto opacity-25`}
+          ></div>
 
           <div>
             <div>
-              <p className={`text-center text-3xl font-jakarta font-bold mt-10 ${darkMode ? "text-white" : "text-black"}`}>
+              <p
+                className={`text-center text-3xl font-jakarta font-bold mt-10 ${
+                  darkMode ? "text-white" : "text-black"
+                }`}
+              >
                 Contact Me
               </p>
-              <span className={`flex justify-center font-jakarta text-sm pt-1 ${darkMode ? "text-white" : ""}`}>
+              <span
+                className={`flex justify-center font-jakarta text-sm pt-1 ${
+                  darkMode ? "text-white" : ""
+                }`}
+              >
                 Feel free to reach out to me anytime!
               </span>
             </div>
             <div className={`flex mt-10 gap-10 items-center justify-center`}>
-              <div className={`w-[30rem] min-h-[40rem] bg-gradient-to-r from-[#0A1D56] to-[#000957] rounded-md  
-                backdrop-filter backdrop-blur-md bg-opacity-10 `}>
+              <div
+                className={`w-[30rem] min-h-[30rem] bg-gradient-to-r from-[#0A1D56] to-[#000957] rounded-md  
+                backdrop-filter backdrop-blur-md bg-opacity-10 `}
+              >
                 <div className="p-10">
                   <div className="flex items-center justify-between">
-                    <h1 className={`font-poppins text-2xl font-bold 
-                        ${darkMode ? "text-white" : "text-black"}`}>Get In Touch</h1>
-                    <img src={ShareContactIcon} className={`w-8 
-                        ${darkMode ? "invert " : ""}`} alt="Share Contact" />
+                    <h1
+                      className={`font-poppins text-2xl font-bold 
+                        ${darkMode ? "text-white" : "text-black"}`}
+                    >
+                      Get In Touch
+                    </h1>
+                    <img
+                      src={ShareContactIcon}
+                      className={`w-8 
+                        ${darkMode ? "invert " : ""}`}
+                      alt="Share Contact"
+                    />
                   </div>
 
-                  <p className={`font-poppins text-sm mt-2 
+                  <p
+                    className={`font-poppins text-sm mt-2 
                       ${darkMode ? "text-white" : "text-black"}
-                  `}>Lorem ipsum dolor sit amet, consectetur adipiscing elit</p>
+                  `}
+                  >
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit
+                  </p>
 
-                  <form onSubmit={handleSubmit} className='font-poppins text-sm'>
+                  <form
+                    onSubmit={handleSubmit}
+                    className="font-poppins text-sm"
+                  >
                     <Input
                       type="text"
                       name="username"
@@ -151,23 +204,27 @@ function Contact() {
                       type="submit"
                       className={`w-full mt-6 py-2 px-4 rounded-md ${
                         darkMode
-                          ? 'bg-blue-600 text-white hover:bg-blue-700'
-                          : 'bg-blue-500 text-white hover:bg-blue-600'
+                          ? "bg-blue-600 text-white hover:bg-blue-700"
+                          : "bg-blue-500 text-white hover:bg-blue-600"
                       } focus:outline-none focus:ring-2 focus:ring-blue-500`}
                     >
                       Submit
                     </button>
                   </form>
-
                 </div>
-                  <div className={`w-60 h-1 rounded-full mx-auto ${darkMode ? "bg-white" : ""}`}></div>
+                <div
+                  className={`w-60 h-1 rounded-full mx-auto ${
+                    darkMode ? "bg-white" : ""
+                  }`}
+                ></div>
 
-                  <div>
-                    <Connect
+                <div>
+                  <Connect
                     darkMode={darkMode}
                     platforms={platforms}
-                    />
-                  </div>
+                    link={link}
+                  />
+                </div>
               </div>
 
               <div className={`w-[30rem] h-[35rem] bg-white rounded-lg`}></div>
