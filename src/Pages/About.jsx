dc1b969a2b256318f3daa09507a2cf063f655a63
Bibintanggg <bintangyudhaa8@gmail.com>
2025-02-13 23:42:51 +0700

    new card

--- src/Pages/About.jsx
+++ src/Pages/About.jsx
@@ -9,6 +9,7 @@ import TechIcon from "../assets/Icon/tech.svg";
 import FrameworkIcon from "../assets/Icon/framework.svg";
 import ToolsIcon from "../assets/Icon/tools.svg";
 import { useDarkMode } from "../Component/DarkMode.jsx";
+import SpotlightCard from "../Component/SpotlightCard.jsx";
 
 import BootstrapIcon from "../assets/TechIcon/Bootstrap.svg";
 import CSS3Icon from "../assets/TechIcon/CSS3.svg";
@@ -96,8 +97,9 @@ function About() {
   }
   return (
     <div
-    className={`relative min-h-screen flex transition-all duration-300 ${darkMode ? "bg-[#131523]" : "bg-[#f2f2fc]"
-    } ${isOpen ? "" : "-ml-[17rem]"}`}
+      className={`relative min-h-screen flex transition-all duration-300 ${
+        darkMode ? "bg-[#131523]" : "bg-[#f2f2fc]"
+      } ${isOpen ? "" : "-ml-[17rem]"}`}
     >
       <div className="w-[250px] h-screen fixed top-0 left-0">
         <Sidebar isOpen={isOpen} setIsOpen={setIsOpen} />
@@ -105,42 +107,65 @@ function About() {
 
       <div className="flex-1 ml-[250px] p-8 md:p-8 overflow-y-auto">
         <div className="bg-transparent">
-        <div className={`flex justify-between text-left ${isOpen ? 'opacity-100' : 'opacity-100'}`}>
-            <h1 className={`text-base font-semibold ${darkMode ? 'text-white' : 'text-gray-800'} 
-            ${isOpen ? "ml-10" : "ml-10"} font-poppins pt-2`}>
+          <div
+            className={`flex justify-between text-left ${
+              isOpen ? "opacity-100" : "opacity-100"
+            }`}
+          >
+            <h1
+              className={`text-base font-semibold ${
+                darkMode ? "text-white" : "text-gray-800"
+              } 
+            ${isOpen ? "ml-10" : "ml-10"} font-poppins pt-2`}
+            >
               ABOUT
             </h1>
             <div className={`text-right ${isOpen ? "" : "mr-10"}`}>
-              <h2 className={`text-base font-semibold ${darkMode ? 'text-white' : 'text-gray-800'} font-poppins`}>
+              <h2
+                className={`text-base font-semibold ${
+                  darkMode ? "text-white" : "text-gray-800"
+                } font-poppins`}
+              >
                 Hello, Bintang !
               </h2>
-              <p className={`-translate-y-2 ${darkMode ? 'text-gray-300' : 'text-gray-600'}`}>
+              <p
+                className={`-translate-y-2 ${
+                  darkMode ? "text-gray-300" : "text-gray-600"
+                }`}
+              >
                 Role &gt; Student
               </p>
             </div>
           </div>
-          <div className={`w-full h-0.5 ${darkMode ? 'bg-white' : 'bg-black'} mx-auto opacity-25`}></div>
+          <div
+            className={`w-full h-0.5 ${
+              darkMode ? "bg-white" : "bg-black"
+            } mx-auto opacity-25`}
+          ></div>
 
           <div className="flex flex-col items-center h-auto gap-5 p-10">
             <div className="flex flex-col md:flex-row items-center gap-5 w-full">
               <div
                 className={`w-full md:w-[60rem] h-[19rem] items-center mx-auto isolate aspect-video 
-                rounded-xl shadow-lg ring-1 ring-black/5 ${darkMode
+                rounded-xl shadow-lg ring-1 ring-black/5 ${
+                  darkMode
                     ? "bg-gradient-to-r from-black/20 to-black/20 "
                     : "bg-white/20"
-                  } ${isOpen ? "" : "h-[46rem] "} `}
+                } ${isOpen ? "" : "h-[46rem] "} `}
               >
                 <div className="p-6 flex items-center gap-4">
                   <h1
-                    className={`font-bold text-xl font-jakarta ${darkMode ? "text-white" : "text-black"
-                      }`}
+                    className={`font-bold text-xl font-jakarta ${
+                      darkMode ? "text-white" : "text-black"
+                    }`}
                   >
                     About Me
                   </h1>
                   <div className="relative flex items-center">
                     <div
-                      className={`absolute w-24 h-0.5 left-1 ${darkMode ? "bg-white" : "bg-black"
-                        }`}
+                      className={`absolute w-24 h-0.5 left-1 ${
+                        darkMode ? "bg-white" : "bg-black"
+                      }`}
                     ></div>
                   </div>
                 </div>
@@ -149,8 +174,9 @@ function About() {
                     <img src={FotoBintang} className="w-48 h-48 rounded-full" />
                     <div className="pt-3">
                       <p
-                        className={`max-w-lg tracking-wide text-base ${darkMode ? "text-white" : "text-black"
-                          } ${isOpen ? "" : "text-balance"}`}
+                        className={`max-w-lg tracking-wide text-base ${
+                          darkMode ? "text-white" : "text-black"
+                        } ${isOpen ? "" : "text-balance"}`}
                       >
                         <span className="ml-10 font-jakarta">Hello There!</span>
                         I'm Bintang Yudha Putra Purnomo, a creative front-end
@@ -158,8 +184,9 @@ function About() {
                         the bustling city of Jakarta, Indonesia.
                       </p>
                       <p
-                        className={`max-w-lg tracking-wide mt-5 text-base ${darkMode ? "text-white" : "text-black"
-                          }`}
+                        className={`max-w-lg tracking-wide mt-5 text-base ${
+                          darkMode ? "text-white" : "text-black"
+                        }`}
                       >
                         <span className="ml-10 font-jakarta">
                           I'm passionate
@@ -176,71 +203,84 @@ function About() {
 
             <div
               className={`w-full md:w-[60rem] h-48 mt-10 flex gap-10 items-center justify-center
-              isolate aspect-video rounded-xl shadow-lg ${darkMode
+              isolate aspect-video rounded-xl shadow-lg ${
+                darkMode
                   ? "bg-gradient-to-r from-black/30 to-black/10"
                   : "bg-white"
-                }`}
+              }`}
             >
               {dataProject.map((project) => (
-                <div
+                <SpotlightCard
                   key={project.id}
                   className={`p-4 rounded-lg h-36 w-full md:w-[17rem] 
-                  isolate aspect-video shadow-lg ${darkMode
+                  isolate aspect-video shadow-lg ${
+                    darkMode
                       ? "bg-gradient-to-r from-black/30 to-black/20 "
                       : "bg-[#f2f2fc]"
-                    }`}
+                  }`}
+                  spotlightColor="rgba(0, 229, 255, 0.2)"
                 >
-                  <div className="cursor-pointer" 
-                  onClick={() => handleNavigate(project.path)}>
-                    <div className="flex items-center gap-5">
+                  <div
+                    className="cursor-pointer -mt-5"
+                    onClick={() => handleNavigate(project.path)}
+                  >
+                    <div className="flex items-center gap-36">
                       <div
                         className={`w-14 h-14 rounded-full justify-center items-center flex 
-                        ${darkMode
+                        ${
+                          darkMode
                             ? "bg-gray-50/20 invert brightness-75"
-                            : "bg-slate-50"
-                          }`}
+                            : "bg-gray-700 rounded-full bg-clip-padding backdrop-filter backdrop-blur-xl bg-opacity-30 "
+                        }`}
                       >
-                        <img src={project.icon} />
+                        <img
+                          src={project.icon}
+                          className={`${darkMode ? "" : "invert"}`}
+                        />
                       </div>
                       <p
-                        className={`font-poppins ${darkMode ? "text-white" : "text-black"
-                          }`}
+                        className={`font-poppins text-white ${
+                          darkMode ? "" : ""
+                        }`}
                       >
                         {project.value}
                       </p>
                     </div>
                     <p
-                      className={`text-sm translate-y-3 font-poppins ${darkMode ? "text-white" : "text-black"
-                        }`}
+                      className={`text-sm translate-y-3 font-poppins ${
+                        darkMode ? "text-white" : "text-white"
+                      }`}
                     >
                       {project.title}
                     </p>
-                    <div className="translate-y-4 font-poppins flex gap-5">
+                    <div className="translate-y-4 font-poppins flex gap-5 mb-">
                       <span
-                        className={`text-[0.65rem] ${darkMode ? "text-white" : "text-black"
-                          }`}
+                        className={`text-[0.65rem] ${
+                          darkMode ? "text-white" : "text-white"
+                        }`}
                       >
                         {project.desc}
                       </span>
-                      <img src={ShareIcon} alt="" className="w-3 opacity-45" />
+                      <img src={ShareIcon} alt="" className="w-3 opacity-45 invert" />
                     </div>
-
                   </div>
-                </div>
+                </SpotlightCard>
               ))}
             </div>
 
             <div>
               <div className="text-center">
                 <p
-                  className={`text-xl font-bold mt-10 font-poppins ${darkMode ? "text-white" : "text-black"
-                    }`}
+                  className={`text-xl font-bold mt-10 font-poppins ${
+                    darkMode ? "text-white" : "text-black"
+                  }`}
                 >
                   Tech Stack
                 </p>
                 <span
-                  className={`text-sm font-jakarta ${darkMode ? "text-white" : "text-black"
-                    }`}
+                  className={`text-sm font-jakarta ${
+                    darkMode ? "text-white" : "text-black"
+                  }`}
                 >
                   Lorem ipsum dolor sit amet consectetur adipisicing elit.
                 </span>
@@ -250,23 +290,26 @@ function About() {
                 <div className="items-center justify-center flex pt-5">
                   <div
                     className={`w-24 h-14 rounded-lg flex justify-center items-center 
-                    isolate aspect-video ${darkMode
+                    isolate aspect-video ${
+                      darkMode
                         ? "bg-gradient-to-r from-black/30 to-black/20"
                         : "bg-white"
-                      }`}
+                    }`}
                   >
                     <div
                       className={`w-20 h-10 rounded-lg flex justify-center items-center backdrop-blur-md backdrop-brightness-75
-                      ${darkMode
+                      ${
+                        darkMode
                           ? "bg-gradient-to-r from-black/30 to-black/10"
                           : "bg-gray-100"
-                        }`}
+                      }`}
                     >
                       <img
                         src={TechIcon}
                         alt="Tech"
-                        className={`w-7 ${darkMode ? "invert brightness-75" : "text-black"
-                          }`}
+                        className={`w-7 ${
+                          darkMode ? "invert brightness-75" : "text-black"
+                        }`}
                       />
                     </div>
                   </div>
@@ -275,28 +318,32 @@ function About() {
                 <div className="flex items-center justify-center mt-2">
                   <button
                     className={`flex items-center justify-center gap-11 w-[360px] h-[65px] rounded-lg 
-                  isolate aspect-video shadow-lg ${darkMode
-                        ? "bg-gradient-to-r from-black/30 to-black/10"
-                        : "bg-white"
-                      }`}
+                  isolate aspect-video shadow-lg ${
+                    darkMode
+                      ? "bg-gradient-to-r from-black/30 to-black/10"
+                      : "bg-white"
+                  }`}
                   >
                     {stack.map((items) => (
                       <div
                         key={items.id}
                         onClick={() => setActiveStack(items.title)}
                         className={`flex items-center gap-2 w-36 h-10 rounded-md 
-                          cursor-pointer isolate aspect-video shadow-lg ${activeStack === items.title ? "bg-gray-100" : ""
-                          } ${darkMode
+                          cursor-pointer isolate aspect-video shadow-lg ${
+                            activeStack === items.title ? "bg-gray-100" : ""
+                          } ${
+                          darkMode
                             ? "text-white bg-gradient-to-r from-black/50 to-black/30"
                             : "text-black bg-gray-100 "
-                          }`}
+                        }`}
                       >
                         <div className="flex items-center justify-center mx-auto gap-2">
                           <img
                             src={items.icon}
                             alt=""
-                            className={`w-6 h-6 ${darkMode ? "invert brightness-75" : ""
-                              }`}
+                            className={`w-6 h-6 ${
+                              darkMode ? "invert brightness-75" : ""
+                            }`}
                           />
                           <p className="text-sm">{items.title}</p>
                         </div>
@@ -313,9 +360,10 @@ function About() {
                     >
                       <div
                         className={`relative flex items-center justify-center w-48 h-6 rounded p-10 gap-3 
-                          overflow-hidden group transition-all duration-300 ease-in-out backdrop-blur-md backdrop-brightness-75 ${darkMode
-                            ? "bg-gradient-to-r from-black/30 to-black/20 border border-white/10"
-                            : "bg-white"
+                          overflow-hidden group transition-all duration-300 ease-in-out backdrop-blur-md backdrop-brightness-75 ${
+                            darkMode
+                              ? "bg-gradient-to-r from-black/30 to-black/20 border border-white/10"
+                              : "bg-white"
                           }`}
                       >
                         <div className="absolute top-0 left-0 h-full bg-gray-200 w-0 transition-all duration-500 group-hover:w-[100%]"></div>
@@ -328,8 +376,9 @@ function About() {
                           />
                           <span
                             className={`text-xs mt-2 font-poppins transition-transform duration-700
-                          ease-in-out group-hover:translate-x-2 ${darkMode ? "text-white" : "text-black"
-                              }`}
+                          ease-in-out group-hover:translate-x-2 ${
+                            darkMode ? "text-white" : "text-black"
+                          }`}
                           >
                             {icons.name}
                           </span>
