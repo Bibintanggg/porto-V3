80b5a28e928f8a53361cfd9fc267d82a28c44cd9
Bibintanggg <bintangyudhaa8@gmail.com>
2025-02-18 22:54:53 +0700

    1/3 finished

--- src/Component/Preload.jsx
+++ src/Component/Preload.jsx
@@ -28,46 +28,49 @@ function Preload({ onLoaded }) {
     return (
       <AnimatePresence>
         {isVisible && (
-          <motion.div
-            className="fixed inset-0 flex items-center justify-center bg-gradient-to-r 
-      from-[#160a1f] to-[#131523]"
-            initial={{ opacity: 1 }}
-            animate={{ opacity: 1 }}
-            exit={{ opacity: 0 }}
-            transition={{ duration: 1 }}
-          >
-            <Terminal className="p-4 w-fit max-w-md text-white">
-              <TypingAnimation>&gt; Initializing Portfolio...</TypingAnimation>
+          <div className="fixed inset-0 flex items-center justify-center bg-[#131523]">
+            <motion.div
+              className=""
+              initial={{ opacity: 1 }}
+              animate={{ opacity: 1 }}
+              exit={{ opacity: 0 }}
+              transition={{ duration: 1 }}
+            >
+              <Terminal className="p-4 w-fit max-w-md text-white">
+                <TypingAnimation>
+                  &gt; Initializing Portfolio...
+                </TypingAnimation>
 
-              <AnimatedSpan delay={1500} className="text-green-500">
-                <span>✔ Setting up the framework... Found React.js</span>
-              </AnimatedSpan>
+                <AnimatedSpan delay={1500} className="text-green-500">
+                  <span>✔ Setting up the framework... Found React.js</span>
+                </AnimatedSpan>
 
-              <AnimatedSpan delay={2000} className="text-green-500">
-                <span>✔ Validating Tailwind CSS.</span>
-              </AnimatedSpan>
+                <AnimatedSpan delay={2000} className="text-green-500">
+                  <span>✔ Validating Tailwind CSS.</span>
+                </AnimatedSpan>
 
-              <AnimatedSpan delay={2500} className="text-green-500">
-                <span>✔ Loading creative animations....</span>
-              </AnimatedSpan>
+                <AnimatedSpan delay={2500} className="text-green-500">
+                  <span>✔ Loading creative animations....</span>
+                </AnimatedSpan>
 
-              <AnimatedSpan delay={3000} className="text-green-500">
-                <span>✔ Validating import alias.</span>
-              </AnimatedSpan>
+                <AnimatedSpan delay={3000} className="text-green-500">
+                  <span>✔ Validating import alias.</span>
+                </AnimatedSpan>
 
-              <AnimatedSpan delay={3500} className="text-green-500">
-                <span>✔ Writing components.json.</span>
-              </AnimatedSpan>
+                <AnimatedSpan delay={3500} className="text-green-500">
+                  <span>✔ Writing components.json.</span>
+                </AnimatedSpan>
 
-              <AnimatedSpan delay={4000} className="text-green-500">
-                <span>✔ Updating tailwind.config.ts</span>
-              </AnimatedSpan>
+                <AnimatedSpan delay={4000} className="text-green-500">
+                  <span>✔ Updating tailwind.config.ts</span>
+                </AnimatedSpan>
 
-              <TypingAnimation delay={4500} className="text-muted-foreground">
-                Success! Portfolio is ready to explore.
-              </TypingAnimation>
-            </Terminal>
-          </motion.div>
+                <TypingAnimation delay={4500} className="text-muted-foreground">
+                  Success! Portfolio is ready to explore.
+                </TypingAnimation>
+              </Terminal>
+            </motion.div>
+          </div>
         )}
       </AnimatePresence>
     );
